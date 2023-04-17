3050cf404851d03d357ca5c7b1e87bae25200c4c
Jose <ynujose@gmail.com>
2023-04-17 13:19:02 -0400

    DJANGO PORTFOLIO TEST

---
+++ README.md
@@ -0,0 +1,14 @@
+# Python Portfolio Test with Django
+***
+This is a portfolio test made with django and python to build a dynamic portfolio and views
+
+Requirements:
+
+A list of technologies used within the project:
+* [Python 3.x](https://www.python.org/)
+* [Django](https://www.djangoproject.com/)
+* [Sqlite](https://sqlite.org/index.html)
+
+
+
+
---
+++ webapp/portfolio/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='portfimages')
+    link = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
